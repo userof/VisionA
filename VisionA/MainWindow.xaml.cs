e66d0cdc9e3728e4cd0fc605c5751c07f4c0233a
roman.steblivskiy <roman.steblivskiy@newground.com.ua>
2016-07-21 13:03:39 +0300

    ok

--- VisionA/MainWindow.xaml.cs
+++ VisionA/MainWindow.xaml.cs
@@ -23,6 +23,75 @@ namespace VisionA
         public MainWindow()
         {
             InitializeComponent();
+            RandomLatter();
+        }
+
+        private void PlusButton_Click(object sender, RoutedEventArgs e)
+        {
+            PlusFont();
+        }
+
+        private void PlusFont()
+        {
+            var fs = VisionLetter.FontSize;
+            fs = Math.Round(fs + (fs/100*10));
+            VisionLetter.FontSize = fs;
+            LatterFonetSize.Text = fs.ToString();
+        }
+
+        private void RundomButton_Click(object sender, RoutedEventArgs e)
+        {
+            RandomLatter();
+        }
+
+        private void RandomLatter()
+        {
+            var str = "QWERTYUIOPASDFGHJKLZXCVBNM";
+            Random rnd = new Random();
+            var r = rnd.Next(0, str.Length - 1);
+            VisionLetter.Text = str[r].ToString();
+        }
+
+        private void MinusButton_Click(object sender, RoutedEventArgs e)
+        {
+            MinusFont();
+        }
+
+        private void MinusFont()
+        {
+            var fs = VisionLetter.FontSize;
+            fs = Math.Round(fs - (fs/100*10));
+            VisionLetter.FontSize = fs;
+            LatterFonetSize.Text = fs.ToString();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.D1:
+                    PlusFont();
+                    break;
+                case Key.D2:
+                    MinusFont();
+                    break;
+                case Key.D3:
+                    RandomLatter();
+                    break;
+                default:
+                    var k = e.Key.ToString();
+                    if (k == VisionLetter.Text)
+                    {
+                        LatterFonetSize.Background = Brushes.Green;
+                        RandomLatter();
+                    }
+                    else
+                    {
+                        LatterFonetSize.Background = Brushes.Red;
+                    }
+                    break;
+
+            }
         }
     }
 }
